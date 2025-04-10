bc67b2c2c7fba9dd3bb33296ae78850fea7477e2
carfornc <carfornc@LCB-10>
2025-04-10 12:54:02 -0400

    added packages and documentation

---
+++ Spyhnx_Assignment11/utilitiesPackage/utilities.py
@@ -0,0 +1,12 @@
+# File Name : utilities.py
+# Student Name: David Becker & Nikki Carfora
+# email:  beckerd8@mail.uc.edu  carfornc@mail.uc.edu
+# Assignment Number: Assignment 11
+# Due Date:   4/17/2025
+# Course #/Section:   IS 4010 001
+# Semester/Year:   Spring 2025
+# Brief Description of the assignment:  
+# Brief Description of what this module does. calls the functions from the other modules and executes them. 
+# Citations: Gemini AI
+
+# Anything else that's relevant:
