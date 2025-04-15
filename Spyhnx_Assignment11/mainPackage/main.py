7600457b2263dc53f394374cc044ea95016bd7d1
David Becker <beckerd8@mail.uc.edu>
2025-04-14 22:17:14 -0400

    Revert "Taken care of gross price, duplicate rows, Pepsi and zip codes. Needs EC still."
    This reverts commit 65dbe56a06a97d345f3d0efe7b3612e4184ba15e.

--- Spyhnx_Assignment11/mainPackage/main.py
+++ Spyhnx_Assignment11/mainPackage/main.py
@@ -5,16 +5,25 @@
 # Due Date:   4/17/2025
 # Course #/Section:   IS 4010 001
 # Semester/Year:   Spring 2025
-# Brief Description of the assignment: Using python to clean the data in a csv file (fix missing values, truncate numbers, delete duplicate rows)
+# Brief Description of the assignment:  Using python to clean the data in a csv file (fix missing values, truncate numbers, delete duplicate rows)
 
 # Brief Description of what this module does. calls the functions from the other modules and executes them. 
 # Citations: 
 
 # Anything else that's relevant:
 
-from dataProcessing.dataProcessing import *
+from utilitiesPackage.utilities import *
+
+class CSVUpload:
+    def __init__(self):
+        csv_reader = CSVReader()
+        self.data = csv_reader.read_CSV_file("./csvData/fuelPurchaseData.csv")
+        self.print_data()
+
+    def print_data(self):
+        for row in self.data:
+            print(row) 
 
 if __name__ == "__main__":
-    processor = DataProcessor("./csvData/fuelPurchaseData.csv")
-    processor.clean_data()
+    fulePurchase = CSVUpload()
 
